b3f6d23b9329e21685a322d7c4c23bf3f96e3a2f
Pablo Bermejo Garcia <pablo@Pablos-MacBook-Pro-16.local>
2026-01-04 09:26:02 +0100

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -89,6 +90,7 @@ export default function RootLayout({
         <ThemeToggle />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
